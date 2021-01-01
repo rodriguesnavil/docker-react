ed612cf37bec34948333feaeb71b546bcf9655c5
Navil <rodriguesnavil@gmail.com>
2021-01-01 23:58:11 +0530

    testing deployment on AWS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo! Whats up!
+          We are on AWS! Wohooo!!
         </p>
         <a
           className="App-link"
